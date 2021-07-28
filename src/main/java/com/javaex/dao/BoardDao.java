8b08889f37a5ac3a6d8c79d5ce5e77101e68cb13
김민수 <minsu.kim1988@gmail.com>
2021-07-29 00:00:22 +0900

    29/July/2021 Spring_framework Board

---
+++ src/main/java/com/javaex/dao/BoardDao.java
@@ -0,0 +1,74 @@
+package com.javaex.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.javaex.vo.BoardVo;
+
+@Repository
+public class BoardDao {
+
+	//field
+	@Autowired
+	private SqlSession sqlSession;
+	
+	//
+	public List<BoardVo> getBoardList(String searchword) {
+		System.out.println("[BaordDao.getBoardList()]");
+		
+		List<BoardVo> boardList = sqlSession.selectList("board.getBoardList", searchword);
+		
+		return boardList;
+	}
+	
+	//조회수 올리기
+	public int updateHit(int no) {
+		System.out.println("[Boardao.updateHit()]");
+		//System.out.println(no);
+		
+		int count = sqlSession.update("board.updateHit", no);
+		
+		return count;
+	}
+	
+	//게시판1개 정보 가져오기
+	public BoardVo selectBoard(int no) {
+		System.out.println("[BoardDao.selectBoard()]");
+		//System.out.println(no);
+
+		BoardVo boardVo = sqlSession.selectOne("board.selectBoard",no);
+		//System.out.println(boardVo);
+		
+		return boardVo;
+	}
+	
+	//write
+	public int writeBoard(BoardVo boardVo) {
+		System.out.println("[BoardDao.writeBoard()]");
+		
+		int count = sqlSession.insert("board.writeBoard", boardVo);
+		
+		return count;
+	}
+	
+	//delete
+	public int deleteBoard(int no) {
+		System.out.println("[BoardDao.deleteBoard()]");
+		
+		int count = sqlSession.delete("board.deleteBoard", no);
+		
+		return count;
+	}
+	
+	//modify
+	public int modifyBoard(BoardVo boardVo) {
+		System.out.println("[BoardDao.modifyBoard()]");
+		
+		int count = sqlSession.update("board.modifyBoard", boardVo);
+		
+		return count;
+	}
+}
---
+++ src/main/java/com/javaex/service/BoardService.java
@@ -0,0 +1,53 @@
+package com.javaex.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.javaex.dao.BoardDao;
+import com.javaex.vo.BoardVo;
+
+@Service
+public class BoardService {
+
+	@Autowired
+	private BoardDao boardDao;
+	
+	public BoardVo getBoard(int no) {
+		System.out.println("[BoardService.getBoard()]");
+		System.out.println(no);
+		
+		//조회수 올리기
+		boardDao.updateHit(no);
+		
+		//게시판 정보 가져오기
+		BoardVo boardVo = boardDao.selectBoard(no);
+		
+		return boardVo;
+	}
+	
+	public List<BoardVo> getBoardList(String searchword){
+		
+		return boardDao.getBoardList(searchword);
+	}
+	
+	public int write(BoardVo boardVo) {
+		System.out.println("[BoardService.writeBoard()]");
+		
+
+		return boardDao.writeBoard(boardVo);
+	}
+	
+	public int delete(int no) {
+		System.out.println("[BoardService.deleteBoard()]");
+		
+		return boardDao.deleteBoard(no);
+	}
+	
+	public int modify(BoardVo boardVo) {
+		System.out.println("[BoardService.modify()]");
+
+		return boardDao.modifyBoard(boardVo);
+	}
+}
